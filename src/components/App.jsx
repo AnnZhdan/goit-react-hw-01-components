88ff2fd25b85b23b79767ca11ddf57a3731ca9af
AnnZhdan <anna.zhdan.work@gmail.com>
2023-12-22 03:29:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import { Profile } from './Profile/Profile.jsx';
-import { Statistics } from './Statistics/Statistics.jsx';
-import { FriendList } from './FriendList/FriendList.jsx';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import data from './data/data.json';
 import friends from './data/friends.json';
